efd0db397c597b40e5463ec3e359fef6c806aa21
Chethan L <chethan.shantha@gmail.com>
2023-06-19 21:50:19 +0530

    minor changes in variable name.

--- Analysis/selection_sort_analysis.py
+++ Analysis/selection_sort_analysis.py
@@ -3,28 +3,36 @@ import time
 import matplotlib.pyplot as plt
 
 
-def selection_sort(list):
-    for i in range(len(list)):
+def selection_sort(arr):
+    for i in range(len(arr)):
         min_index = i
-        for j in range(i+1, len(list)):
-            if list[j] < list[min_index]:
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[min_index]:
                 min_index = j
-        list[i], list[min_index] = list[min_index], list[i]
-    return list
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
 
 
 n = [0, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000]
 time_taken = []
 
 for i in n:
-    list = [random.randint(0, 1000000) for i in range(i)]
+    arr = [random.randint(0, 1000000) for i in range(i)]
     start = time.time()
-    selection_sort(list)
+    selection_sort(arr)
     end = time.time()
     time_taken.append((end-start)*1000)
+
+
+print("Selection Sort")
+print("No. of elements:\tTime taken in milliseconds")
+for i in range(len(n)):
+    print(n[i], "\t\t\t", time_taken[i])
+
+
 plt.title("Selection Sort")
 plt.xlabel("Number of elements")
-plt.xlim(0, 5000)
+plt.xlim(0, 5500)
 plt.ylabel("Time taken in milliseconds(ms)")
 plt.plot(n, time_taken)
 plt.show()
